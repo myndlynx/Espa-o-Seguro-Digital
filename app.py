d0f4a6b3ea783557bcbbbd9181ff3016e4afa670
Mynd Lynx <brunna.ranna@academico.ifpb.edu.br>
2026-05-02 22:20:08 -0300

    ui papai

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
-app = Flask (__name__)
+
+app = Flask(__name__)
 
 @app.route('/discente/login')
-def login ():
-  return render_template('login_dis.html')
+def login():
+    return render_template('login_dis.html')
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
